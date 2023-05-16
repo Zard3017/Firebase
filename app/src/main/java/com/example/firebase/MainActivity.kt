c7e5ef049c453f22c14dd072b8dcb7b92b987b33
Zard3017 <ianbradley3017@gmail.com>
2023-05-16 09:29:02 +0300

    initial commit

---
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.firebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    lateinit var EdtEmail:EditText
+    lateinit var Edtpass:EditText
+    lateinit var Btnsignup:Button
+    lateinit var TvDirectLogin:TextView
+    lateinit var Edtconfirmpass:EditText
+    lateinit var auth:FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        EdtEmail=findViewById(R.id.etSEmailAddress)
+        Edtpass=findViewById(R.id.etSPassword)
+        Edtconfirmpass=findViewById(R.id.etSConfPassword)
+        Btnsignup=findViewById(R.id.btnSSigned)
+        TvDirectLogin=findViewById(R.id.tvRedirectLogin)
+        auth= Firebase.auth
+
+        TvDirectLogin.setOnClickListener {
+            val intent=Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+        Btnsignup.setOnClickListener {
+            SignUpUser()
+
+        }
+    }
+    private fun SignUpUser(){
+        val email=EdtEmail.text.toString()
+        val pass=Edtpass.text.toString()
+        val confirmpass=Edtconfirmpass.text.toString()
+
+        if (email.isBlank() ||pass.isBlank()||confirmpass.isBlank()){
+            Toast.makeText(this, "Please email and password cannot be blank",Toast.LENGTH_LONG).show()
+            return
+        }else if (pass!=confirmpass){
+            Toast.makeText(this, "password do not match",Toast.LENGTH_LONG).show()
+            return
+        }
+        auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(this){
+            if (it.isSuccessful){
+                Toast.makeText(this,"Signed successfully",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Failed to create", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+}
